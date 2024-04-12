3157fe15cd0fd877d321267e7cdd8fcaab9998e0
pakeh2866 <lushan2866@gmail.com>
2024-04-12 13:26:48 +0800

    更新

---
+++ ld_qiandao.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         链滴每日签到
+// @namespace    pakeh2866
+// @version      0.1
+// @description  链滴每日签到
+// @author       pakeh2866
+// @match        https://ld246.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ld246.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
+
+function qianDao(){
+    
+}
