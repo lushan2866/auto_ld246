5439d8e31590a5b488fadf26f7d5b18d22df52d5
pakeh2866 <lushan2866@gmail.com>
2024-04-15 20:43:16 +0800

    合并冲突

--- ld_qiandao.js
+++ ld_qiandao.js
@@ -4,22 +4,38 @@
 // @version      0.1
 // @description  链滴每日签到
 // @author       pakeh2866
-// @match        https://ld246.com/
+// @match        https://ld246.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ld246.com
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-
+    window.onload = onloadStart()
     // Your code here...
 })();
 
-function qianDao(){
-    
+//判断是否登录
+function checkLogin() {
+    var checkLogin = document.getElementsByClassName('avatar-small');
+    console.log(checkLogin);
+    if (checkLogin) {
+      isLogin = true;
+    }}
+
+// 执行绿色签到click
+function qd_button(){
+   var button_click = document.getElementsByClassName("btn green");
+   console.log(button_click[0])
+   button_click[0].click();
 }
 
-function issign90{
+function onloadStart(){
+    checkLogin()
+    //qd_button()
+}
+
+function issign(){
    var signOutElement = document.getElementById('signOut');
 
     // 如果该元素存在，则将issignin的值设为true
@@ -30,3 +46,4 @@ function issign90{
     // 输出issignin的值，用于验证结果
     console.log('issignin:', issignin);
 }
+
