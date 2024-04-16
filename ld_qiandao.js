d8b8501b4cbd6f7eb8c3a77375e49531983fb688
pakeh2866 <lushan2866@gmail.com>
2024-04-16 13:52:59 +0800

    Update ld_qiandao.js

--- ld_qiandao.js
+++ ld_qiandao.js
@@ -2,7 +2,7 @@
 // @name         链滴每日签到
 // @namespace    pakeh2866
 // @version      0.1
-// @description  链滴每日签到
+// @description  每日第一次签到
 // @author       pakeh2866
 // @match        https://ld246.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ld246.com
